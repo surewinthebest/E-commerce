e4d1ac06224ade0ff750181fba13f2a4418beeda
surewinthebest <somucus6@gmail.com>
2026-07-09 21:01:18 +0800

    [Feat]mobile app setup

---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, Image } from 'react-native'
+
+const AuthScreen = () => {
+  return (
+    <View >
+      <Text>AuthScreen</Text>
+      <Image src="../../assets/images/auth-image.png"/>
+    </View>
+  )
+}
+export default AuthScreen
---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,23 @@
+import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import "../global.css";
+
+const queryClient = new QueryClient();
+
+export default function RootLayout() {
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+  
+  if (!publishableKey) {
+    throw new Error('Add your Clerk Publishable Key to the .env file')
+  }
+
+  return
+  <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }} />;
+    </QueryClientProvider>
+  </ClerkProvider>
+}
